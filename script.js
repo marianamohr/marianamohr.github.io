da3d9b7123fea89de1140dd43eea378367ad7aed
Mariana Mohr <marianamohr@gmail.com>
2021-03-25 09:59:17 -0300

    ajustado menu para versao mobile

---
+++ script.js
@@ -0,0 +1,19 @@
+const menu = document.getElementById('icone-menu-mobile');
+
+function abrirMenu() {
+
+    const menuSuspenso = document.querySelector('.teste');
+
+    if (menuSuspenso.classList.contains('menu-suspenso') === true) {
+
+        menuSuspenso.classList.remove('menu-suspenso');
+    }
+    else if(menuSuspenso.classList.contains('menu-suspenso') === false) {
+
+        menuSuspenso.classList.add('menu-suspenso');
+    }
+
+}
+
+
+menu.addEventListener('click', abrirMenu);
